f82d85a47322b78c2758693d4357c62585b6fb65
harsha9726 <harsha9726@gmail.com>
2021-08-31 11:12:25 +0530

    Corrected DB function and added extract script for region data

--- app/utils/DB_functions.py
+++ app/utils/DB_functions.py
@@ -4,7 +4,8 @@ from .DB import db_fetch, db_execute
 
 async def get_region_ids():
     query = """select region_id from region"""
-    result = await db_fetch(query, is_one=True)
+    result = await db_fetch(query, is_one=False)
+    return result
 
 async def insert_region_data(region):
     query = """insert into region(constellations ,description, name, region_id)
